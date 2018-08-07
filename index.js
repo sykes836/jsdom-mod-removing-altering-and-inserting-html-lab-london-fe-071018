f9866f85719f473d4e10d32d2cebeff126a3f628
sykes836 <bernard.sykes836@gmail.com>
2018-08-07 18:19:58 +0000

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
 /* Enter the code to remove the main node element under this comment */
 
 /* Create your new element here and assign it to newHeader */
-const newHeader = null;
+
+main.remove ()
+
+const newHeader = document.createElement('h1');
+newHeader.id = "victory";
+document.body.appendChild(newHeader);
+
+  var newDiv = document.createElement("h1#victory"); 
+  var newContent = document.createTextNode("Bernard is Champion"); 
+  newDiv.appendChild(newContent);
+  document.body.appendChild(element)
+
+
